9e628a0c825fc4a80fac0f21ad4e97ceb6e097fd
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-22 19:50:55 +0530

    [sanjana] Add : Stack and Oueue

---
+++ StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to stack", value);
+        }
+    }
+}
