94bdedaa13e1bbf64fb97a62cbce8f49e991d792
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-22 20:10:00 +0530

    [sanjana] Add : Stack Peek and pop Opertion

--- StackAndQueue/LinkedListStack.cs
+++ StackAndQueue/LinkedListStack.cs
@@ -36,5 +36,24 @@ namespace StackAndQueue
                 temp = temp.next;
             }
         }
+        public void Peek()
+        {
+            if(this.top == null)
+            {
+                Console.WriteLine("stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the TOP of the stack",this.top);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is Empty");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
     }
 }
