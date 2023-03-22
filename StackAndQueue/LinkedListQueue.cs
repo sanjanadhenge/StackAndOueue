cd44771ff2fc9e674875cf2add822536ca8c7bb0
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-22 20:23:57 +0530

    [sanjana] Add : Oueue Enqueu

---
+++ StackAndQueue/LinkedListQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    internal class LinkedListQueue
+    {
+        private Node head;
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is enqueued to Queue", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Oueue is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -11,13 +11,20 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             LinkedListStack stack = new LinkedListStack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.Display();
+           // stack.Push(70);
+           // stack.Push(30);
+           // stack.Push(56);
+          //  stack.Display();
+           // stack.Peek();
+          //  stack.Pop();
+           // stack.Display();
+           LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
+
 
 
         }
